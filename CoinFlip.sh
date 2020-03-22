2646812b60beab2acd2fa8d619f2e58afa464271
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-22 01:42:11 -0700

    Usecase4

--- CoinFlip.sh
+++ CoinFlip.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 
 
+simulator () {
+	if [ $headCount -eq 21 -a $flag -eq 0 ]
+	then
+		flag=1
+		arr[0]=Head 
+		arr[1]=$((headCount-tailCount))
+		return 
+	elif [ $tailCount -eq 21 -a $flag -eq 0 ] 
+	then
+		flag=1
+		arr[0]=Tails 
+		arr[1]=$((tailCount-headCount))
+		return 
+	elif [ $headCount -ge $((tailCount+2)) -a $flag -eq 1 ]
+	then
+		arr[2]=Head
+		return
+	elif [ $tailCount -ge $((headCount+2)) -a $flag -eq 1 ]
+	then
+		arr[2]=Tails
+		return
+	fi
+		r=$((RANDOM%2))
+		if [ $r -eq  1 ]
+		then
+			headCount=$((headCount+1))
+		else
+			tailCount=$((tailCount+1))
+		fi
+		simulator
+}
+
+checkWinner () {
+
+	if [ $headCount -eq 21 -a $tailCount -ne 21 ]
+	then
+		echo "Heads Won by" $((headCount-tailCount)) "point"
+	elif [ $tailCount -eq 21 -a $headCount -ne 21 ]
+	then
+		echo "Tails Won by" $((tailCount-headCount)) "point"
+	elif [ $headCount -eq 21 -a $tailCount -eq 21 ]
+	then
+		echo "Its a Tie"
+		simulator
+	fi
+
+}
+
 headCount=0
 tailCount=0
-while [ $headCount -lt 21 -a $tailCount -lt 21 ]
-do
-	r=$((RANDOM%2))
-	if [ $r -eq  1 ]
-	then
-		headCount=$((headCount+1))
-	else
-		tailCount=$((tailCount+1))
-	fi
-done
-
-if [ $headCount -eq 21 -a $tailCount -ne 21 ]
-then
-	echo "Heads Won by" $((headCount-tailCount)) "point"
-elif [ $tailCount -eq 21 -a $headCount -ne 21 ]
-then
-	echo "Tails Won by" $((tailCount-headCount)) "point"
-elif [ $headCount -eq 21 -a $tailcount -eq 2 ]
-then
-	echo "Its a Tie"
-fi
-
+flag=0
+simulator
+echo ${arr[0]} "won by" ${arr[1]} "points"
+[[ "${arr[0]}" == "Head" ]] && tailCount=`expr $tailCount + ${arr[1]}` || headCount=`expr $headCount + ${arr[1]}`
+checkWinner
+echo "Final Winner is "${arr[2]}
